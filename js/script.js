9405ea97fa24d8980d507b33a0c28d9fe6603a7c
yudi marga <yudi.marga@gmail.com>
2024-10-21 00:10:46 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,148 @@
+//Toggle Navbar class Active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Mouse klik diluar navbar dan hamburger menu untuk menutup navbar
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// Slider next & prev
+
+const slider = document.querySelector(".slider");
+const list = document.querySelector(".list");
+const thumbnail = document.querySelector(".thumbnail");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+
+// autoplay
+let runAutoPlay = setTimeout(() => {
+  next.click();
+}, 20000);
+
+next.addEventListener("click", () => {
+  initSlider("next");
+});
+
+prev.addEventListener("click", () => {
+  initSlider("prev");
+});
+
+const initSlider = (type) => {
+  const sliderItems = list.querySelectorAll(".item");
+  const thumbnailItems = thumbnail.querySelectorAll(".item");
+
+  if (type === "next") {
+    list.appendChild(sliderItems[0]);
+    thumbnail.appendChild(thumbnailItems[0]);
+    slider.classList.add("next");
+  } else {
+    const lastItemPosition = sliderItems.length - 1;
+    list.prepend(sliderItems[lastItemPosition]);
+    thumbnail.prepend(thumbnailItems[lastItemPosition]);
+    slider.classList.add("prev");
+  }
+
+  setTimeout(() => {
+    slider.classList.remove("next");
+    slider.classList.remove("prev");
+  }, 2000);
+
+  clearTimeout(runAutoPlay);
+  runAutoPlay = setTimeout(() => {
+    next.click();
+  }, 8000);
+};
+
+// counterdown
+
+// let valueDisplays = document.querySelectorAll(".num");
+// let interval = 5000;
+
+// valueDisplays.forEach((valueDisplay) => {
+//   let startValue = 0;
+//   let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+//   let duration = Math.floor(interval / endValue);
+//   let counter = setInterval(function () {
+//     startValue += 1;
+//     valueDisplay.textContent = startValue;
+//     if (startValue == endValue) {
+//       clearInterval(counter);
+//     }
+//   }, duration);
+// });
+
+$(document).ready(function () {
+  $(".counter").counterUp({
+    delay: 10,
+    time: 500,
+  });
+});
+
+// Modal Box 1
+
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    btn.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Klik Tombol close modal
+
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// Javascript Konsultan
+let preveiwContainer = document.querySelector(".products-preview");
+let previewBox = preveiwContainer.querySelectorAll(".preview");
+
+document.querySelectorAll(".products-container .product").forEach((product) => {
+  product.onclick = () => {
+    preveiwContainer.style.display = "flex";
+    let name = product.getAttribute("data-name");
+    previewBox.forEach((preview) => {
+      let target = preview.getAttribute("data-target");
+      if (name == target) {
+        preview.classList.add("active");
+      }
+    });
+  };
+});
+
+previewBox.forEach((close) => {
+  close.querySelector(".fa-times").onclick = () => {
+    close.classList.remove("active");
+    preveiwContainer.style.display = "none";
+  };
+});
+
+// tombol load more di artikel kami
+
+let loadMoreBtn = document.querySelector("#load-more");
+let currentItem = 3;
+
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll(".artikel .box-container .box")];
+  for (var i = currentItem; i < currentItem + 3; i++) {
+    boxes[i].style.display = "inline-block";
+  }
+  currentItem += 3;
+
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = "none";
+  }
+};
